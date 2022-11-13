f206756c9d0ee94115fc5b1694e3f933912f90cc
jak010 <bluetoon@naver.com>
2022-11-13 23:03:54 +0900

    <feat> : Employee Detail API Fix - Employee를 기준으로 연관있는 title을 찾음 - Serializer 레벨에서 Query 수행

--- src/employees/api/employees_detail.py
+++ src/employees/api/employees_detail.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from rest_framework import serializers
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from ..employee_serializers import EmployeeSerializer
 from ..models.EmployeeModel import Employees
 
 
@@ -11,7 +11,12 @@ class EmployeeDetailApi(APIView):
     """ Employee Update Api """
 
     def get(self, request, emp_no: int):
-        """ employee 업데이트 api """
-        employee: Employees = Employees.objects.get(emp_no=emp_no)
+        """ employee 상세조회 api """
 
-        return Response(data=employee.to_dict())
+        employee = Employees.objects.filter(emp_no=emp_no) \
+            .first()
+
+        # prefetch_related가 아닌 DRF Serializer에서 쿼리를 수행한다.
+        serializer = EmployeeSerializer(employee)
+
+        return Response(data=serializer.data)
---
+++ src/employees/employee_serializers.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from rest_framework import serializers
+
+from .models.EmployeeModel import Employees, Titles
+
+
+class TitlesSeializer(serializers.ModelSerializer):
+    class Meta:
+        model = Titles
+        fields = '__all__'
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    titles_set = TitlesSeializer(many=True)
+
+    class Meta:
+        model = Employees
+        fields = ['__all__',
+                  'titles_set']
+
+    def to_representation(self, instance):
+        return {
+            'emp_no': instance.emp_no,
+            'first_name': instance.first_name,
+            'last_name': instance.last_name,
+            'hire_date': instance.hire_date.isoformat(),
+            'birth_date': instance.birth_date.isoformat(),
+            'titles': [{
+                'title_name': item.title,
+                'from_date': item.from_date.isoformat(),
+                'to_date': item.to_date.isoformat(),
+            } for item in instance.titles_set.order_by('-to_date')]
+        }
